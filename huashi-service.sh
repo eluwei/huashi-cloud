3b189a335e817c66e78b598852a03b13cb46cd8d
laingkunJoin <542218419@qq.com>
2018-07-12 16:43:29 +0800

    上传实体

--- huashi-service.sh
+++ huashi-service.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+jarFileName=huashi-cloud-customer-0.1-SNAPSHOT.jar
+cloudRoot=/opt/project/cloud
+
+
 ssh root@47.106.145.4 "cd /opt/scripts/cloud; chmod +x huashi.sh; ./huashi.sh" 
 
